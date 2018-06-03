a9b6e1f7ea94b25b1745a54011d43de4eb959955
João Antunes <joaofbantunes@gmail.com>
2018-06-03 19:16:41 +0100

    Creating a test project and adding a generate.bat.

---
+++ samples/CodingMilitia.GrpcExtensions.ScopedRequestHandlerClientSample/generate.bat
@@ -0,0 +1,7 @@
+﻿rmdir /S /Q Generated
+mkdir Generated
+copy %UserProfile%\.nuget\packages\grpc.tools\1.10.0\tools\windows_x64\protoc.exe .
+copy %UserProfile%\.nuget\packages\grpc.tools\1.10.0\tools\windows_x64\grpc_csharp_plugin.exe .
+protoc service.proto --csharp_out ./Generated/. --grpc_out ./Generated/. --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
+del protoc.exe
+del grpc_csharp_plugin.exe
---
+++ tests/CodingMilitia.GrpcExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodingMilitia.GrpcExtensions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
